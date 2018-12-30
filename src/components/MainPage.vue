6a097e0a30ea2b2b16577c654391e75d90904415
Simon <simon.wolf.lobo@gmail.com>
2018-12-30 17:43:26 -0500

    Added new api request for posters and ratings, commeted part of code

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <h1>Movies</h1>
-    <b-container fluid>
+    <b-container fluid class="search">
       <b-row>
         <b-col sm="3">
           <label class="big">Search Movies</label>
@@ -10,63 +10,89 @@
           <b-form-input
             type="text"
             v-model="search"
-            placeholder="Enter title, actor, genre or service (Netflix or Amazon Prime)"
+            placeholder="Enter title, actor, director, genre or service (Netflix or Amazon Prime)"
           ></b-form-input>
         </b-col>
       </b-row>
     </b-container>
-    <div v-for="(movie, index) in searchedMovies" :key="index">
-      <h2>{{movie.title}}</h2>
-      <p>
-        Cast:
-        <span v-for="(actor, idx) in movie.starring" :key="idx">
-          {{actor}}
-          <span v-if="movie.starring.length -1 != idx">,</span>
-        </span>
-      </p>
-      <p>{{movie.desc}}</p>
-      <p>
-        Genre:
-        <span class="genre" v-for="(genre, index) in movie.genre" :key="index">
-          {{genre}}
-          <span v-if="movie.genre.length -1 != index">,</span>
-        </span>
-      </p>
-      <p>Director: {{movie.director}}</p>
-      <p v-for="(stream, index) in movie.available" :key="index">Available at: {{stream}}</p>
-      <a :href="movie.url" target="_blank">
-        <p>IMDb Link</p>
-      </a>
-    </div>
+    <b-card-group>
+      <div v-for="(movie, index) in searchedMovies" :key="index" class="movie">
+        <b-card
+          no-body
+          style="max-width: 20rem"
+          :footer="'Genre: '+movie.genre"
+          :img-src="getPoster(movie.title)"
+          img-alt="Movie poster"
+          img-top
+        >
+          <h4 slot="header">{{movie.title}}</h4>
+          <b-card-body>
+            <p class="card-text">{{movie.desc}}</p>
+          </b-card-body>
+          <b-list-group flush>
+            <b-list-group-item v-for="(cast, index) in movie.starring" :key="index">{{cast}}</b-list-group-item>
+            <b-list-group-item>
+              <span class="dir">Director:</span>
+              {{movie.director}}
+            </b-list-group-item>
+            <b-list-group-item>
+              Available:
+              <span v-for="(stream, i) in movie.available" :key="i">{{stream+" "}}</span>
+            </b-list-group-item>
+          </b-list-group>
+          <b-card-body>
+            IMBb:
+            <a :href="movie.url" class="card-link" target="_blank">{{movie.title}}</a>
+            <p>
+              Tomato score:
+              <span class="rating">{{getRating(movie.title)}}</span>
+            </p>
+          </b-card-body>
+        </b-card>
+      </div>
+    </b-card-group>
   </div>
 </template>
 
 <script>
+// Import axios for ajax requests
 import axios from "axios";
 export default {
   name: "MainPage",
   data() {
     return {
+      // data
       movies: [],
-      search: ""
+      search: "",
+      poster: [],
+      ratings: []
     };
   },
+  // Funcions called after instance has been mounted
   mounted() {
+    // Get all the movies and store them in an array
     axios({
       method: "GET",
       url: "https://lobonode.ddns.net/api"
     }).then(res => {
       this.movies = res.data;
+      // Run function for each movie in the database
+      this.movies.forEach(title => {
+        this.fillIMDb(title.title);
+      });
     });
   },
+  // Computed functions
   computed: {
+    // Search function
     searchedMovies: function() {
       let filter = new RegExp(this.search, "i");
       let final;
+      // Filter array
       return this.movies.filter(movie => {
+        // check title,actor, director, stream service and genre
         final = movie.title.match(filter);
         final += movie.director.match(filter);
-
         for (let i = 0; i < movie.available.length; i++) {
           final += movie.available[i].match(filter);
         }
@@ -76,9 +102,67 @@ export default {
         for (let i = 0; i < movie.genre.length; i++) {
           final += movie.genre[i].match(filter);
         }
-
+        // return filtered array
         return final;
       });
+    }
+  },
+  // Functions
+  methods: {
+    // get imdb data
+    fillIMDb: function(title) {
+      // Ajax get call to API omdbapi
+      /** example part of response
+         * {"Title":"Star Wars: The Last Jedi",
+         * "Year":"2017","Rated":"PG-13","Released":"15 Dec 2017",
+         * "Runtime":"152 min","Genre":"Action, Adventure, Fantasy, Sci-Fi",
+         * "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg",
+         * "Ratings":[{"Source":"Internet Movie Database","Value":"7.2/10"},{"Source":"Rotten Tomatoes","Value":"91%"},
+         * {"Source":"Metacritic","Value":"85/100"}],
+
+         */
+      axios({
+        method: "GET",
+        url: "http://www.omdbapi.com/?t=" + title + "&apikey=dabd7b02"
+      }).then(res => {
+        // Add poster url and ratings from rotten tomatoes to two arrays
+        this.poster.push(title + " " + res.data.Poster);
+        this.ratings.push(title + " " + res.data.Ratings[1].Value);
+      });
+    },
+    // get poster url
+    getPoster: function(title) {
+      var source;
+      // Gag movie
+      if (title == "Natalies Crazy Cats") {
+        source = "https://placekitten.com/g/300/450";
+      } else {
+        // check the array
+        this.poster.forEach(item => {
+          // if it includes title
+          if (item.includes(title)) {
+            // remove title and keep url
+            source = item.split(title + " ").pop();
+          }
+        });
+      }
+      // return url
+      return source;
+    },
+    // same as previous function but for ratings
+    getRating: function(title) {
+      var rating;
+      if (title == "Natalies Crazy Cats") {
+        rating = "100%";
+      } else {
+        this.ratings.forEach(item => {
+          if (item.includes(title)) {
+            rating = item.split(title + " ").pop();
+          }
+        });
+      }
+
+      return rating;
     }
   }
 };
@@ -86,18 +170,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+#main {
+  width: 100%;
+  margin: 0 auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.dir {
+  font-weight: bold;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+body {
+  width: 100%;
+  margin: 0 auto;
 }
-a {
-  color: #42b983;
+h1 {
+  text-align: center;
+}
+.search {
+  margin-bottom: 5%;
+}
+.movie {
+  margin: 1%;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>This site lets you search movies available on Netflix and/or Amazon Prime.</p>
+    <h3>Netflix</h3>
+    <p>
+      The movies available on Netflix apply to the United States only. Some movies may not be available if you do not live in the US.
+      Most Netflix original movies are available worldwide.
+    </p>
+    <h3>Amazon Prime</h3>
+    <p>The movies for this site refrences the US Amazon Prime Video service. So they may not be available if you own a subscription outside the US.</p>
   </div>
 </template>
